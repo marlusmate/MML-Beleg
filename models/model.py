712cea120bc5ff764ec22be33d3db194e7071d06
marlusmate <mpj_esser@t-online.de>
2022-04-15 12:02:10 +0200

    Generalize Modelling Workflow, better suited for working with branches

--- models/model.py
+++ models/model.py
@@ -11,7 +11,7 @@ from tensorflow.keras import Model
 from tensorflow import float32
 
 
-class LeNet:
+class mmlmodel:
     @staticmethod
     def build(input_shape, classes):
         # Model initialisation
@@ -39,102 +39,3 @@ class LeNet:
 
         # We need to return the model structure to make it integrable
         return model
-
-
-class MLP1:
-    @staticmethod
-    def build(input_shape, classes):
-        # Input
-        input_params = Input(shape=input_shape, name="InputMLP1")
-
-        # First complex of fully connected Dense Layers (dense, activation)
-        x = Dense(30, name="Dense1MLP1")(input_params)
-        x = Activation("relu", name="Relu1MlP1")(x)
-
-        # Second complex of fully connected Dense Layers (dense, activation)
-        x = Dense(15, name="Dense2MLP1")(x)
-        x = Activation("relu", name="Relu2MlP1")(x)
-
-        # Third complex of fully connected Dense Layers (dense, activation)
-        x = Dense(15, name="Dense3MLP1")(x)
-        x = Activation("relu", name="Relu3MlP1")(x)
-
-        # Fourth Complex of Classification Layer (output)
-        output = Dense(classes, activation="softmax", name="OutputMLP1")(x)
-
-        return Model(input_params, output)
-
-
-class MMMLP1:
-    @staticmethod
-    def build_image_extraction(input_shape):
-        # Input
-        input_image = Input(shape=input_shape, name="InputImageMMMLP1")
-
-        # First complex of convolutional layers (conv, activation, pooling)
-        x = Conv2D(filters=20, kernel_size=(7, 7), padding="same", name="conv1MMMLP1")(input_image)
-        x = Activation("relu", name="relu1ImgMMMLP1")(x)
-        x = MaxPooling2D(pool_size=(2, 2), strides=(2, 2), name="maxpool1MMMLP1")(x)
-
-        # Second complex of convolutional layers (conv, activation, pooling)
-        x = Conv2D(filters=15, kernel_size=(5, 5), padding="same", name="conv2MMMLP1")(x)
-        x = Activation("relu", name="relu2ImgMMMLP1")(x)
-        x = MaxPooling2D(pool_size=(2, 2), strides=(2, 2), name="maxpool2MMMLP1")(x)
-
-        # Third complex of convolutional layers (conv, activation, pooling)
-        x = Conv2D(filters=10, kernel_size=(5, 5), padding="same", name="conv2MMMLP1")(x)
-        x = Activation("relu", name="relu2ImgMMMLP1")(x)
-        x = MaxPooling2D(pool_size=(2, 2), strides=(2, 2), name="maxpool2MMMLP1")(x)
-
-        # Output Layer (flatten)
-        output_img = Flatten(name="outputIamgeMMMLP1")(x)
-
-        return Model(inputs=input_image, outputs=output_img, name="ModelImageExtract")
-
-    @staticmethod
-    def build_params_extraction(input_shape):
-        # Input
-        input_params = Input(shape=input_shape, name="InputProcessMMMLP1")
-
-        # First complex of fully connected Dense Layers (dense, activation)
-        x = Dense(20, name="dense1ParamMMMLP1")(input_params)
-        x = Activation("relu", name="relu1ParamMMMlP1")(x)
-
-        # Second complex of fully connected Dense Layers (dense, activation)
-        x = Dense(15, name="dense2ParamMMMLP1")(x)
-        x = Activation("relu", name="relu2ParamMMlP1")(x)
-
-
-        # Output
-        output_params = Dense(15, name="OutputParamMMMLP1")(x)
-
-        return Model(inputs=input_params, outputs=output_params, name="ModelParamExtract")
-
-    @staticmethod
-    def build_fusion(input_shape_image, input_shape_params, no_classes):
-        # Input
-        image_in = Input(shape=input_shape_image, name="FrontInputImageMMMLP1")
-        params_in = Input(shape=input_shape_params, name="FrontInputParamsMMMLP1")
-
-        # Build Feature extraction blocks (Image, Process Parameters)
-        cnn1 = MMMLP1.build_image_extraction(input_shape_image)
-        param1 = MMMLP1.build_params_extraction(input_shape_params)
-
-        # First complex of fully connected Layers (concatenate)
-        merged = Concatenate()([cnn1(image_in), param1(params_in)])
-
-        # Second complex of fully connected Layers (dense, activation
-        x = Dense(15, name="dense1MergedMMMLP1")(merged)
-        x = Activation("relu", name="relu1MergedMMMlP1")(x)
-        x = Dropout(rate=0.2)(x)
-
-
-        # Second complex of fully connected Dense Layers (dense, activation)
-        x = Dense(15, name="dense2MergedMMMLP1")(x)
-        x = Activation("relu", name="relu2MergedMMlP1")(x)
-        x = Dropout(rate=0.2)(x)
-
-        # Output
-        output = Dense(no_classes, activation="sigmoid", name="OutputMergedMMMLP1")(x)
-
-        return Model(inputs=(image_in, params_in), outputs=output, name="ModelMultimodal")
--- training/model_training.py
+++ training/model_training.py
@@ -1,5 +1,5 @@
 import os
-from models.model import LeNet
+from models.model import mmlmodel
 from tensorflow.keras.optimizers import Adam
 import tensorflow as tf
 import random
@@ -11,11 +11,15 @@ from data_import.data_import import get_data_points_list, image_generator, get_e
 # Disable CUDA devices
 #os.environ["CUDA_VISIBLE_DEVICES"] = "1"
 print(tf.config.list_physical_devices('GPU'))
+
+# Model Name
+model_name = "LeNet20x50"
+
 # Path to data
 data_folder = 'mnt/0A60B2CB60B2BD2F/Datasets/bioreactor_flow_regimes_me/02_data'
 
 # Path to where save model
-checkpoint_path = './results/checkpointsMMMLP1/checkpoint-{epoch:04d}.ckpt'
+checkpoint_path = f'./results/checkpoints{model_name}/checkpoint-{epoch:04d}.ckpt'
 
 # Path to save logs for tensorboard
 tensorboard_log_folder = './resultsMMMLP1/tensorboard'
@@ -51,6 +55,14 @@ data_gen_train = image_generator(data_points_train, no_epochs, no_classes, outpu
 dataset_train = tf.data.Dataset.from_generator(lambda: data_gen_train, output_signature=output_signature)
 dataset_train_batched = dataset_train.batch(batch_size)
 
+lb_train = []
+for data_point in data_points_train:
+    with open(data_point[1]) as f:
+        json_content = json.load(f)
+        lb_train.append(json_content["flow_regime"]["data"]["value"])
+print("Trainings Instanzen:\n-----------------------")
+print("Klasse 0: ", lb_train.count(0), "\nKlasse 1: ", lb_train.count(1), "\nKlasse 2: ", lb_train.count(2))
+
 # Validation dataset
 no_val_points = int(split_ratio[1] / sum(split_ratio) * dataset_len)
 data_points_val = shuffled_data_points[no_train_points:no_train_points + no_val_points]
@@ -59,23 +71,30 @@ data_gen_val = image_generator(data_points_train, no_epochs, no_classes, output_
 dataset_val = tf.data.Dataset.from_generator(lambda: data_gen_val, output_signature=output_signature)
 dataset_val_batched = dataset_val.batch(batch_size)
 
+lb_val = []
+for data_point in data_points_val:
+    with open(data_point[1]) as f:
+        json_content = json.load(f)
+        lb_val.append(json_content["flow_regime"]["data"]["value"])
+print("Validierungs Instanzen:\n-----------------------")
+print("Klasse 0: ", lb_val.count(0), "\nKlasse 1: ", lb_val.count(1), "\nKlasse 2: ", lb_val.count(2))
+
 # Test dataset
 no_test_points = int(split_ratio[2] / sum(split_ratio) * dataset_len)
 data_points_test = shuffled_data_points[no_train_points + no_val_points:]
-
+"""
 data_gen_test = image_generator(data_points_test, no_epochs, no_classes, output_img_shape, param_list)
 dataset_test = tf.data.Dataset.from_generator(lambda: data_gen_test, output_signature=output_signature)
 dataset_test_batched = dataset_test.batch(batch_size)
-
+"""
 # Save Data-points for later testing
 with open('data-points-test.pickle', 'w') as f:
     pickle.dump(data_points_test, f)
-print("Saved Data points for testing; n=", len/data_points_test)
-
+print("\nSaved Data points for testing; n=", len/data_points_test)
 
 # Model compilation
 opt = Adam(learning_rate=init_lr, decay=init_lr / no_epochs)
-model = LeNet.build(input_shape=output_img_shape, classes=no_classes)
+model = mmlmodel.build(input_shape=output_img_shape, classes=no_classes)
 model.compile(loss="categorical_crossentropy", optimizer=opt, metrics=["accuracy"])
 model.summary()
 print("Model mit CUDA compiliert: ", tf.test.is_built_with_cuda())
@@ -113,9 +132,9 @@ history = model.fit(dataset_train_batched,
                     )
 
 # Saving model
-model.save('./results/trained_modelMMMLP1')
+model.save(f'./results/trained_model{model_name}')
 
 # Print and save on disk model training history
-with open('./results/reportMMMLP1.json', 'w', encoding='utf-8') as f:
+with open(f'./results/report{model_name}', 'w', encoding='utf-8') as f:
     json.dump(history.history, f, ensure_ascii=False, indent=4)
 
